5ba4ab8a0b0234c8a748070820bb6323e1887a7b
Aakash Shah <mraakashshah@users.noreply.github.com>
2026-05-31 09:20:59 -0400

    test(web): remove unused css helper

--- pkg/web/css_test.go
+++ pkg/web/css_test.go
@@ -71,18 +71,6 @@ func TestCSSContainsKeySelectors(t *testing.T) {
 	}
 }
 
-func contains(s, substr string) bool {
-	return len(s) >= len(substr) && (s == substr || len(substr) == 0 ||
-		func() bool {
-			for i := 0; i <= len(s)-len(substr); i++ {
-				if s[i:i+len(substr)] == substr {
-					return true
-				}
-			}
-			return false
-		}())
-}
-
 func TestCSSOverflowSafety(t *testing.T) {
 	css := readDashboardCSS(t)
 
